526e2f82f9d5fc5c6c5dce39e82872ba7c41896b
Baramykin <baramykin0@gmail.com>
2022-06-11 22:36:03 +0600

    Cange Layers to Presets

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('path')
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 550,
+    width: 560,
     height: 640,
     resizable: false, // Lock resizable of windows
     icon: __dirname + '/images.png', // set favicon
